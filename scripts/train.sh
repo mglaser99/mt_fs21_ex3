ecd00d41ce104df9f51e6c7438375be9eb9aa622
mglaser99 <melanie.glaser2@uzh.ch>
2021-05-04 10:37:31 +0200

    small updates

--- scripts/train.sh
+++ scripts/train.sh
@@ -17,8 +17,8 @@ SECONDS=0
 (cd $tools/pytorch-examples/word_language_model &&
     CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python main.py --data $data/Jokes \
         --epochs 40 \
-        --emsize 250 --nhid 250 --dropout 0.4 --tied \
-        --save $models/jokes_model_0.4.pt
+        --emsize 250 --nhid 250 --dropout 0.5 --tied \
+        --save $models/jokes_model_05.pt
 ) 
 
 echo "time taken:"
